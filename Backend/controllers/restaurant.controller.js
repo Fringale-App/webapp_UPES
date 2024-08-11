cb80d03ce32a811f2664101835696ef807bdd493
shubham.mishra <shubham.mishra@enercross.com>
2024-08-11 18:35:37 +0530

    added get restaurant,update restaurant,get restaurants functionality with testing

--- Backend/controllers/restaurant.controller.js
+++ Backend/controllers/restaurant.controller.js
@@ -8,3 +8,60 @@ export const registerRes = async (req,res,next)=>{
         next(err)
     }
 }
+export const getRestaurant = async (req, res, next) => {
+    try {
+      const restaurant = await Restaurant.findById(req.params.id);
+      if (!restaurant) {
+        return next();
+      }
+      res.status(200).json(restaurant);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  export const updateRestaurant = async (req, res, next) => {
+    const restaurant = await Restaurant.findById(req.params.id);
+    if (!restaurant) {
+      return next();
+    }
+    // if (req.user.id !== restaurant.userRef) {
+    //   return next();
+    // }
+  
+    try {
+      const updatedRestaurant = await Restaurant.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+      res.status(200).json(updatedRestaurant);
+    } catch (error) {
+      next(error);
+    }
+ };  
+
+ export const getRestaurants = async (req, res, next) => {
+    try {
+      const limit = parseInt(req.query.limit) || 9;
+      const startIndex = parseInt(req.query.startIndex) || 0;
+  
+      const searchTerm = req.query.searchTerm || '';
+  
+      const sort = req.query.sort || 'createdAt';
+  
+      const order = req.query.order || 'desc';
+  
+      const Restaurants = await Restaurant.find({
+        name: { $regex: searchTerm, $options: 'i' }
+        
+      })
+        .sort({ [sort]: order })
+        .limit(limit)
+        .skip(startIndex);
+  
+      return res.status(200).json(Restaurants);
+    } catch (error) {
+      next(error);
+    }
+  };
--- Backend/routes/restaurant.route.js
+++ Backend/routes/restaurant.route.js
@@ -1,6 +1,9 @@
 import express from 'express'
-import { registerRes } from '../controllers/restaurant.controller.js'
+import { registerRes ,getRestaurant ,getRestaurants,updateRestaurant} from '../controllers/restaurant.controller.js'
 
 const router = express.Router()
 router.post('/create',registerRes)
+router.get('/get/:id',getRestaurant)
+router.get('/get',getRestaurants)
+router.post('/update/:id',updateRestaurant)
 export default router
