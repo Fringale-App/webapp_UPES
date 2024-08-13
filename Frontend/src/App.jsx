6669ba76220f23ad2db2fab11fc81eeef0f03718
shubham.mishra <shubham.mishra@enercross.com>
2024-08-14 01:01:57 +0530

    added search layout

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,16 @@
 // src/App.jsx
 import React from 'react';
 import Header from './components/Header';
+import Search from './components/Search';
 
 function App() {
   return (
     <div>
       <Header/>
+      <div className='p-4'>
+        <Search/>
+      </div>
+      
     </div>
   );
 }
--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -5,16 +5,22 @@ import { RiUserLine } from "react-icons/ri";
 
 function Header() {
   return (
-    <div
-      className="flex justify-between items-center py-4 px-4"
-      style={{ boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.25)' }}
-    >
-      <TfiHeart className="text-[rgba(0,100,60,1)] h-[40px] w-[40px]"/>
-      <div className='h-[40px] w-[157px] overflow-hidden object-cover' >
-        <img className="w-full h-full" src={logo} alt="" />
+    <div>
+      <div
+        className="flex justify-between items-center py-4 px-4"
+        style={{ boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.25)' }}
+      >
+        <TfiHeart className="text-[rgba(0,100,60,1)] h-[40px] w-[40px] transform scale-110" />
+        <div className='h-[40px] w-[157px] overflow-hidden object-cover' >
+          <img className="w-full h-full" src={logo} alt="" />
+        </div>
+        <RiUserLine className="text-[rgba(0,100,60,1)] h-[40px] w-[40px] transform scale-110" />
       </div>
-      <RiUserLine className="text-[rgba(0,100,60,1)] h-[40px] w-[40px]" />
+     
+    
+
     </div>
+
   );
 }
 
