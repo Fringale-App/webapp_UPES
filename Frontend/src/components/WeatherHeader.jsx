562ec77885f8a6ddbf5a89918cd6ce2606a00d93
shubham.mishra <shubham.mishra@enercross.com>
2024-08-14 23:33:02 +0530

    added weather functionality

--- Frontend/src/components/WeatherHeader.jsx
+++ Frontend/src/components/WeatherHeader.jsx
@@ -1,15 +1,73 @@
-import React,{useState} from "react";
+import React,{useEffect, useState} from "react";
 import { FaSearch, FaHeart, FaUserCircle } from "react-icons/fa";
 import logo from '../../Images/logo.png';
 import heart from '../../Images/heart.svg';
 import profile from '../../Images/profile.svg';
 import weather from '../../Images/weather.png'
+import clear_icon from '../../Images/clear.png'
+
 
 const WeatherHeader = () => {
     const [value,setValue] = useState('');
+    const [weatherData , setWeatherData] = useState(false)
+    const [dateTime, setDateTime] = useState(new Date());
+    const allIcons = {
+      "01d":"https://openweathermap.org/img/wn/01d@2x.png",
+      "01n":"https://openweathermap.org/img/wn/01n@2x.png",
+      "02d":"https://openweathermap.org/img/wn/02d@2x.png",
+      "02n":"https://openweathermap.org/img/wn/02n@2x.png",
+      "03d":"https://openweathermap.org/img/wn/03d@2x.png",
+      "03n":"https://openweathermap.org/img/wn/03n@2x.png",
+      "04d":"https://openweathermap.org/img/wn/04d@2x.png",
+      "04n":"https://openweathermap.org/img/wn/04n@2x.png",
+      "09d":weather,
+      "09n":weather,
+      "10d":weather,
+      "10n":weather,
+      "13d":"https://openweathermap.org/img/wn/13d@2x.png",
+      "13n":"https://openweathermap.org/img/wn/13n@2x.png",
+      
+
+    }
+    const search = async (city)=>{
+      try{
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=110df3663b2a8e49823808bfc64cdd2f`
+        console.log(url)
+        const response = await fetch(url)
+        const data = await response.json()
+        const currIcon = allIcons[data.weather[0].icon] || clear_icon
+        console.log(data)
+        setWeatherData({
+          humidity:data.main.humidity,
+          windSpeed:data.wind.speed,
+          temperature:Math.floor(data.main.temp),
+          location:data.name,
+          icon:currIcon,
+          condition:data.weather[0].main
+
+        })
+      } catch(error){
+        console.log(error+"hello")
+      }
+    }
+   useEffect(()=>{
+    search("Dehradun")
+    const intervalId = setInterval(() => {
+      setDateTime(new Date());
+    }, 1000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(intervalId);
+   },[])
   const onChange = (e) => {
     setValue(e.target.value);
   }
+  const optionsDate = { weekday: 'short', day: '2-digit', month: 'short' };
+  const formattedDate = dateTime.toLocaleDateString('en-GB', optionsDate);
+
+  // Format the time as "H:mm"
+  const optionsTime = { hour: 'numeric', minute: '2-digit', hour12: true };
+  const formattedTime = dateTime.toLocaleTimeString('en-GB', optionsTime);
 //   console.log(value);
   return (
     <div className="p-4 bg-white shadow-md">
@@ -37,12 +95,12 @@ const WeatherHeader = () => {
       </div>
 
       {/* Weather and time section */}
-      <div className="mt-4 p-4 bg-blue-200 rounded-lg flex items-center min-h-[70px] justify-between bg-cover bg-center w-full relative" style={{ backgroundImage: `url(${weather})` }}>
+      <div className="mt-4 p-4 bg-blue-200 rounded-lg flex items-center min-h-[70px] justify-between bg-center w-full relative" style={{ backgroundImage: `url(${weatherData.icon})` }}>
         <div className="absolute top-7">
-          <p className=" text-sm font-bold">Raining 24°C</p>
-          <p className="text-xs font-bold">Sun, 10 Aug</p>
+          <p className=" text-sm font-bold">{weatherData.condition} {weatherData.temperature}°C</p>
+          <p className="text-xs font-bold">{formattedDate}</p>
         </div>
-        <p className="text-sm absolute bottom-2 right-5 font-bold">9:41 PM</p>
+        <p className="text-sm absolute bottom-2 right-5 font-bold">{formattedTime}</p>
       </div>
     </div>
   );
