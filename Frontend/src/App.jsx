5fe3ec970d86ff83c1facaba94e6f0ce8b5e6a8a
shubham.mishra <shubham.mishra@enercross.com>
2024-09-22 09:59:19 +0530

    category created for campuses

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import {BrowserRouter,Routes,Route}from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import WeatherHeader from './components/WeatherHeader';
 import FoodOptions from './components/FoodOptions';
 import Home from './pages/Home';
@@ -9,7 +9,7 @@ import SwipeFilter from './pages/SwipeFilter';
 import Filter from './pages/Filter';
 import Search from './pages/Search';
 import { Provider } from 'react-redux';
-import { store ,persistor } from './redux/store.jsx';
+import { store, persistor } from './redux/store.jsx';
 import { PersistGate } from 'redux-persist/integration/react';
 
 
@@ -23,31 +23,34 @@ import PrivateComponent from './components/PrivateComponent';
 import Profile from './pages/Profile';
 function App() {
   return (
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate loading={null} persistor={persistor}>
-        <Header/>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <Header />
 
-        <Routes>
-          
-          <Route path="/" element={<Home/>}/>
-          <Route path="/swipe-filter" element={<SwipeFilter/>}/>
-          <Route path="/food-filter" element={<Filter/>}/>
-          <Route path="/search" element={<Search/>}/>
-          <Route path="/restaurant/:id" element={<RestaurantPage/>}/>
-          <Route path="/bucket" element={<Bucket/>} />
-          <Route path="/signin" element={<SignIn/>} />
-          <Route path="/signup" element={<SignUp/>} />
-          <Route path="/popup" element={<PopUp/>} />
-          <Route element={<PrivateComponent/>}>
-            <Route path='/profile' element={<Profile/>}/>
-          </Route>
+          <Routes>
 
-          
-        </Routes>
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>
+            <Route path="/" element={<Home />} />
+            <Route path="/bidholi" element={<Home />} />
+            <Route path="/kandholi" element={<Home />} />
+            
+            <Route path="/swipe-filter" element={<SwipeFilter />} />
+            <Route path="/food-filter" element={<Filter />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/restaurant/:id" element={<RestaurantPage />} />
+            <Route path="/bucket" element={<Bucket />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/popup" element={<PopUp />} />
+            <Route element={<PrivateComponent />}>
+              <Route path='/profile' element={<Profile />} />
+            </Route>
+
+
+          </Routes>
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- Frontend/src/components/RestaurantList.jsx
+++ Frontend/src/components/RestaurantList.jsx
@@ -1,23 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import RestaurantCard from './RestaurantCard';
 
-const RestaurantList = () => {
-    const [restaurants,setRestaurants] = useState([])
-    useEffect(()=>{
+const RestaurantList = ({ campus }) => {
+    const kandholi = {
+        cafeFrisco: "66c7534d734efe7b1bf8b59e",
+    };
+
+    const bidholi = {
+        annapurna: "66c7534d734efe7b1bf8b599",
+        bidholiFrisco: "66c7534d734efe7b1bf8b59a",
+        chaiGaramBidholi: "66c7534d734efe7b1bf8b59b",
+        maximus: "66c7534d734efe7b1bf8b59c",
+        theKathiRoll: "66c7534d734efe7b1bf8b59d"
+    };
+
+    const [restaurants, setRestaurants] = useState([]);
+
+    useEffect(() => {
         const fetchRestaurants = async () => {
             try {
-              const restaurantRes = await fetch('http://localhost:3000/api/restaurant/get');
-              const resData = await restaurantRes.json();
-              setRestaurants(resData);
-      
+                const restaurantRes = await fetch('http://localhost:3000/api/restaurant/get');
+                const resData = await restaurantRes.json();
+                setRestaurants(resData);
             } catch (error) {
-              console.log(error);
+                console.log(error);
             }
-          }
-        fetchRestaurants()
-},[])
-    
-
+        };
+        fetchRestaurants();
+    }, []);
 
     return (
         <div className="p-4">
@@ -27,12 +37,36 @@ const RestaurantList = () => {
             </div>
 
             <div className='sm:flex sm:justify-center sm:items-center sm:flex-wrap sm:gap-3'>
-                {restaurants.map((restaurant, index) => (
+                {/* Render All Restaurants */}
+                {campus === 'All' && restaurants.map((restaurant, index) => (
                     <RestaurantCard key={index} restaurant={restaurant} />
                 ))}
+
+                {/* Render Kandholi Restaurants */}
+                {campus === 'Kandholi' && restaurants
+                    .filter((restaurant) => restaurant._id === kandholi.cafeFrisco)
+                    .map((restaurant, index) => (
+                        <RestaurantCard key={index} restaurant={restaurant} />
+                    ))
+                }
+
+                {/* Render Bidholi Restaurants */}
+                {campus === 'Bidholi' && restaurants
+                    .filter((restaurant) =>
+                        restaurant._id === bidholi.annapurna ||
+                        restaurant._id === bidholi.bidholiFrisco ||
+                        restaurant._id === bidholi.chaiGaramBidholi ||
+                        restaurant._id === bidholi.maximus ||
+                        restaurant._id === bidholi.theKathiRoll
+                    )
+                    .map((restaurant, index) => (
+                        <RestaurantCard key={index} restaurant={restaurant} />
+                    ))
+                }
             </div>
         </div>
     );
 };
 
 export default RestaurantList;
+
